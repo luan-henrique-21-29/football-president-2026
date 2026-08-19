758b2cc1f53e1b21df8318d2bf35c515129017ac
luan-henrique-21-29 <luan.aparecido.jesus@escola.pr.gov.br>
2026-08-19 16:43:03 -0300

    Remove risky extra match wrapper from runtime chain

--- src/game-v3.js
+++ src/game-v3.js
@@ -1,4 +1,4 @@
-export * from './game-v13.js';
-import {askingPrice as baseAskingPrice} from './game-v13.js';
+export * from './game-v12.js';
+import {askingPrice as baseAskingPrice} from './game-v12.js';
 import {realisticAskingPrice} from './transfer-pricing.js';
 export function askingPrice(player){return realisticAskingPrice(player,baseAskingPrice(player))}
