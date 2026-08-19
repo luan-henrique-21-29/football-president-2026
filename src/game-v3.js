1d280dc5bf0afb4a2d58ec21e9262479ecdf116e
luan-henrique-21-29 <luan.aparecido.jesus@escola.pr.gov.br>
2026-08-18 22:49:21 -0300

    Route matches through coach autonomy engine

--- src/game-v3.js
+++ src/game-v3.js
@@ -1,4 +1,4 @@
-export * from './game-v9.js';
-import {askingPrice as baseAskingPrice} from './game-v9.js';
+export * from './game-v10.js';
+import {askingPrice as baseAskingPrice} from './game-v10.js';
 import {realisticAskingPrice} from './transfer-pricing.js';
 export function askingPrice(player){return realisticAskingPrice(player,baseAskingPrice(player))}
