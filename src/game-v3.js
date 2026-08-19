43d0c1449bf3e07388b0be4d54498632362d3370
luan-henrique-21-29 <luan.aparecido.jesus@escola.pr.gov.br>
2026-08-19 12:07:56 -0300

    Diagnose gameplay validation path

--- src/game-v3.js
+++ src/game-v3.js
@@ -1,4 +1,4 @@
-export * from './game-v12.js';
-import {askingPrice as baseAskingPrice} from './game-v12.js';
+export * from './game-v11.js';
+import {askingPrice as baseAskingPrice} from './game-v11.js';
 import {realisticAskingPrice} from './transfer-pricing.js';
 export function askingPrice(player){return realisticAskingPrice(player,baseAskingPrice(player))}
