9dba2b1c97d6d4af0b43db8be5d69a53a81d3b94
luan-henrique-21-29 <luan.aparecido.jesus@escola.pr.gov.br>
2026-08-19 01:44:32 -0300

    Route active game engine through v11

--- src/game-v3.js
+++ src/game-v3.js
@@ -1,4 +1,4 @@
-export * from './game-v10.js';
-import {askingPrice as baseAskingPrice} from './game-v10.js';
+export * from './game-v11.js';
+import {askingPrice as baseAskingPrice} from './game-v11.js';
 import {realisticAskingPrice} from './transfer-pricing.js';
 export function askingPrice(player){return realisticAskingPrice(player,baseAskingPrice(player))}
